c2566c88cd5d78a5ad5daade4df0de58d50ed7a0
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2024-04-03 17:44:02 -0500

    Add `@implement` macro.

--- src/ExtendableInterfaces.jl
+++ src/ExtendableInterfaces.jl
@@ -2,58 +2,10 @@
 
 module ExtendableInterfaces
 
+export @implement, @interface
+export requiredmethods, superinterfaces
 
-export @interface, requiredmethods, superinterfaces
-
-
-function requiredmethods end
-function superinterfaces end
-
-
-function interface_helper(name, superinterfaces, methods)
-    superinterface_objs = Expr(:tuple, map(s -> :($s()), superinterfaces.args)...)
-
-    # No error handling yet. For now we assume that `methods` is
-    # a block with a list of function names (Symbols).
-    methods = filter(arg -> arg isa Symbol, methods.args)
-
-    name_str = String(name)
-
-    ex = quote
-        # This will throw an `UndefVarErr` if any of the declared superinterfaces
-        # are not yet defined.
-        $(superinterfaces.args...)
-
-        struct $name
-            methods::Tuple
-            $name() = new(tuple($(methods...)))
-        end
-
-        ExtendableInterfaces.superinterfaces(::$name) = $superinterface_objs
-        ExtendableInterfaces.requiredmethods(I::$name) = I.methods
-
-        Base.show(io::IO, ::$name) = print(io, $name_str, "()")
-        Base.show(io::IO, ::MIME"text/plain", ::$name) = print(io, "Interface: ", $name_str)
-    end
-
-    esc(ex)
-end
-
-
-macro interface(name::Symbol, methods)
-    interface_helper(name, :(()), methods)
-end
-
-
-macro interface(name::Symbol, extends::Symbol, superinterfaces, methods)
-    extends === :extends || error("Invalid interface extension syntax.")
-
-    if superinterfaces isa Symbol
-        superinterfaces = :(($superinterfaces, ))
-    end
-
-    interface_helper(name, superinterfaces, methods)
-end
-
+include("interface.jl")
+include("implement.jl")
 
 end
