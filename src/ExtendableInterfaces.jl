5206b334d1306f8b6a6832cb60216e762e031787
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2025-07-13 08:37:52 -0500

    Move some TODO comments to a file ignored by git.

--- src/ExtendableInterfaces.jl
+++ src/ExtendableInterfaces.jl
@@ -10,12 +10,6 @@ export NoMatchingIDispatchMethodError, SingleArgumentAmbiguityError, MultipleArg
 abstract type Interface end
 abstract type ConcreteInterface end
 
-# TODO: Update `required_methods` so it works on interface intersections?
-# TODO: Add `all_required_methods`?
-# TODO: Add `all_superinterfaces`?
-# TODO: Allow long-form function definitions and keyword arguments in `@idispatch`.
-# TODO: Allow this syntax: `@interface A: foo, bar`, or `@interface A extends B, C: foo, bar`.
-
 include("utils.jl")
 include("interface.jl")
 include("intersection.jl")
