4e7a578919e3baa94b9c97ddf1d207b4c394e441
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2025-07-13 20:01:15 -0500

    Add docstrings in the `interface.jl` file.

--- src/interface.jl
+++ src/interface.jl
@@ -7,6 +7,27 @@ function var"-MultipleInterfaces-superinterfaces-" end
 _superinterfaces(x::ConcreteInterface) = var"-MultipleInterfaces-superinterfaces-"(x)
 
 # The exported version that dispatches on interface types rather than instances.
+"""
+    superinterfaces(I)
+
+Return a tuple of the superinterfaces of the interface `I`.
+
+This function is not defined for interface intersections.
+
+# Examples
+```jldoctest
+julia> @interface A begin sin end
+
+julia> @interface B begin cos end
+
+julia> @interface C extends A, B begin
+           tan
+       end
+
+julia> superinterfaces(C)
+(A, B)
+```
+"""
 function superinterfaces(I::Type{<:ConcreteInterface})
     map(typeof, var"-MultipleInterfaces-superinterfaces-"(I()))
 end
@@ -16,6 +37,31 @@ end
 function var"-MultipleInterfaces-required_methods-" end
 
 # The exported version that dispatches on interface types rather than instances.
+"""
+    required_methods(I)
+
+Return an immutable vector containing the fixed list of required methods
+for interface `I`.
+
+This function is not defined for interface intersections.
+
+# Examples
+```jldoctest
+julia> function foo end;
+
+julia> function bar end;
+
+julia> @interface A begin
+           foo
+           bar
+       end
+
+julia> required_methods(A)
+2-element MultipleInterfaces.RequiredMethodsVector{Function}:
+ foo (generic function with 0 methods)
+ bar (generic function with 0 methods)
+```
+"""
 function required_methods(I::Type{<:ConcreteInterface})
     var"-MultipleInterfaces-required_methods-"(I())
 end
@@ -104,7 +150,70 @@ macro interface(name::Symbol, methods_block::Expr)
     interface_helper(name, :(()), methods_block)
 end
 
+"""
+    @interface(args...)
 
+Define a new interface. An interface definition includes a list of methods
+that must be defined for a type in order for that type to implement this
+interface. Every interface must require at least one method, and all methods
+in the list are mandatory.
+
+Use the following syntax to define a new interface `A` with the required methods
+`foo` and `bar`:
+
+```julia
+@interface A begin
+    foo
+    bar
+end
+
+A new interface can extend one or more existing interfaces. To define a new
+interface `B` that extends the interface `A`, use the following syntax:
+
+```julia
+@interface B extends A begin
+    baz
+    qux
+end
+```
+
+To define a new interface that extends multiple interfaces, provide the extended
+interfaces as a comma separated list, like this:
+
+```julia
+@interface R extends P, Q begin
+    qwer
+    asdf
+end
+```
+
+Note that the functions in the required methods list must already exist or
+the `@interface` macro will throw an `UndefVarErr` (and likewise for the list
+of interfaces that are being extended).
+
+The required methods of an interface can be inspected with the `required_methods`
+function. Interfaces can be dispatched on in special methods that are defined
+with the `@idispatch` macro.
+
+# Examples
+```jldoctest
+julia> function foo end
+foo (generic function with 0 methods)
+
+julia> function bar end
+bar (generic function with 0 methods)
+
+julia> @interface A begin
+           foo
+           bar
+       end
+
+julia> required_methods(A)
+2-element MultipleInterfaces.RequiredMethodsVector{Function}:
+ foo (generic function with 0 methods)
+ bar (generic function with 0 methods)
+```
+"""
 macro interface(name::Symbol, extends::Symbol, superinterfaces::Union{Symbol, Expr}, methods_block)
     if extends !== :extends
         error("Use `extends` syntax to make a subinterface, like `@interface C extends A, B`.")
@@ -146,6 +255,25 @@ _implements(T::Type) = var"-MultipleInterfaces-implements-"(T)
 _implements(::T) where {T} = _implements(T)
 
 # The exported version. Returns interface types rather than instances.
+"""
+    implements(T::Type)
+
+Return a tuple of the interfaces implemented by the type `T`.
+
+# Examples
+```jldoctest
+julia> function a end;
+
+julia> @interface A begin a end
+
+julia> struct Ant end
+
+julia> @type Ant implements A
+
+julia> implements(Ant)
+(A,)
+```
+"""
 implements(T::Type) = map(typeof, _implements(T))
 
 
@@ -178,6 +306,43 @@ function ancestors(interface::ConcreteInterface)
 end
 
 
+"""
+    all_required_methods(I)
+
+Return an immutable vector containing the fixed set of required methods
+in interface `I` and all the superinterfaces of `I`. This provides the
+complete list of methods that must be implemented by a type in order
+to implement the interface `I`.
+
+This function is not defined for interface intersections.
+
+# Examples
+```jldoctest
+julia> function a end;
+
+julia> function b end;
+
+julia> function c end;
+
+julia> @interface A begin a end
+
+julia> @interface B begin b end
+
+julia> @interface C extends A, B begin
+           c
+       end
+
+julia> required_methods(C)
+1-element MultipleInterfaces.RequiredMethodsVector{typeof(c)}:
+ c (generic function with 0 methods)
+
+julia> all_required_methods(C)
+3-element MultipleInterfaces.RequiredMethodsVector{Function}:
+ a (generic function with 0 methods)
+ b (generic function with 0 methods)
+ c (generic function with 0 methods)
+```
+"""
 function all_required_methods(T::Type{<:ConcreteInterface})
     superinterfaces = ancestors(T()) # Never empty, because `T()` is included.
     methods = mapreduce(required_methods ∘ typeof, union, superinterfaces)
@@ -192,6 +357,52 @@ function update_implemented(::Type{T}, new_impls::Tuple) where {T}
 end
 
 
+"""
+    @type(args...)
+
+Declare that a type implements one or more interfaces. To declare that the
+type `Foo` implements the interface `A`, use the following syntax:
+
+```julia
+@type Foo implements A
+```
+
+More than one interface implementation for a type can be declared in the
+same `@type` declaration, by providing a comma-separated list of interfaces,
+like this:
+
+```julia
+@type Bar implements B, C
+```
+
+Note that declaring that a type implements an interface implies that the
+type also implements all the superinterfaces of that interface. For example,
+if `Y` is a subinterface of `X`, then `@type Qux implements Y` declares
+that `Qux` implements both `Y` and `X`.
+
+The interfaces implemented by a type can be queried with the `implements`
+function.
+
+# Examples
+```jldoctest
+julia> function a end;
+
+julia> function b end;
+
+julia> @interface A begin a end
+
+julia> @interface B extends A begin
+           b
+       end
+
+julia> struct Foo end
+
+julia> @type Foo implements B
+
+julia> implements(Foo)
+(B, A)
+```
+"""
 macro type(type, implements::Symbol, interfaces_list_ex)
     type = esc(type)
 
